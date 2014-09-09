e08d54b5437a1f6b7c703daa05b65157d26e0c5d
Peter Downs <peterldowns@gmail.com>
2014-09-09 16:17:55 -0400

    Clean up and refactor code to use fewer callbacks.

--- index.js
+++ index.js
@@ -1,60 +1,50 @@
-//INITIAL SETUP
-
 //Import all packages
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
 var spotifySearch = require('./spotifySearch.js');
-
-
+var port = process.env.PORT || 8081;
+var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8081;
-
+var makeSlackResponse = function(trackList) {
+  var spotifyLink = "https://open.spotify.com/track/";
+  var textResponse = '';
+  for (var i = 0; i < trackList.length; i++) {
+    var track = trackList[i];
+    textResponse += ('<' + spotifyLink + track.id + '|' + track.name + '>\n');
+  }
+  var responseJSON = {'text' : textResponse};
+  console.log(responseJSON);
+  return responseJSON;
+}
 
 var router = express.Router();
-
-router.use(function(req,res, next){
-    console.log('Received a request...');
-    next();
+router.use(function(req, res, next){
+  console.log('Received a request...');
+  next();
 });
-
 router.get('/', function(req,res) {
     res.json({message: 'Welcome!'});
     res.send('hello!');
 });
-router.route('/slotify')
-
-    .post(function (req, res) {
-        var rawRequest = req.body.text;
-        console.log(req.body.text, "this loaded");
-        var artist = rawRequest.substring("slotify ".length -1);
-        console.log(artist);
-        console.log('Searching for artist...');
-        spotifySearch.queryToID(artist, function(ID){
-            spotifySearch.getTopTracks(ID, function(list){
-                res.json(resMaker(list));
-            });
-        });
+router.route('/slotify').post(function(req, res) {
+  var requestString = req.body.text;
+  console.log("req.body.text: " + req.body.text);
+  var slotifyPrefixOffset = ('slotify '.length - 1);
+  var artistName = requestString.substring(slotifyPrefixOffset);
+  console.log('performing a search for: ' + artistName);
+  spotifySearch.getTracksByArtist(artistName, 5).then(function(trackList) {
+    console.log('success!');
+    console.log(arguments);
+    return res.json(makeSlackResponse(trackList));
+  }, function(error) {
+    console.log('failure!');
+    console.log(arguments);
+    return res.json({"error": error});
+  });
 });
 
-function resMaker(trackList) {
-    var spotifyLink = "https://open.spotify.com/track/";
-    var respond = {
-        "text":""
-    };
-    var textRespond = "";
-    for(var i = 0; i < 5; i++){
-        textRespond += ("<"+spotifyLink + trackList[i].id+"|"+trackList[i].name+">"+"\n");
-    }
-    respond.text = textRespond;
-    return respond;
-
-}
-
-
 app.use('/api', router);
-
 app.listen(port);
-console.log('Getting sexy at port ' + port);
+console.log("Started server on " + port);
--- spotifySearch.js
+++ spotifySearch.js
@@ -1,37 +1,41 @@
-var spotifyWebApi = require('spotify-web-api-node');
-var spotifyApi = new spotifyWebApi();
+var spotify = require('spotify-web-api-node');
 
-var region = 'US';
+module.exports = new function() {
+  var spotifyClient = new spotify()
+  var spotifyRegion = 'US';
 
-function spotifySearch(artistQuery, callback){
-    var artistID;
-    spotifyApi.searchArtists(artistQuery)
-        .then(function(data) {
-            artistID = data.artists.items[0].id;
-            callback(artistID);
-        }, function(err) {
-            console.error(err);
-        });
+  var getArtistID = function(artistName) {
+    var artistsPromise = spotifyClient.searchArtists(artistName);
+    return artistsPromise.then(function(data) {
+      // TODO(peter): hamming distance to determine which artist is closest to
+      // the one being searched-for.
+      var artistID = data.artists.items[0].id;
+      return artistID;
+    });
+  };
 
-}
-
-function trackSearch(ID, callback) {
-    spotifyApi.getArtistTopTracks(ID,region)
-        .then(function(data){
-            var topTracks=[];
-            for(var i =0;i < 5; i++){
-                var tracksAdd = {
-                    'name' : data.tracks[i].name,
-                    'id' : data.tracks[i].id
-                };
-                topTracks.push(tracksAdd);
-            }
-            console.log(topTracks);
-            callback(topTracks);
-        }, function(err) {
-            console.log(err);
-        });
-}
-
-module.exports.queryToID= spotifySearch;
-module.exports.getTopTracks= trackSearch;
+  this.getTracksByArtist = function(artistName, numberOfTracks) {
+    return getArtistID(artistName).then(function(artistID) {
+      var tracksPromise = spotifyClient.getArtistTopTracks(
+        artistID, spotifyRegion);
+      return tracksPromise.then(function(data) {
+        var topTracks = [];
+        console.log('requested ' + numberOfTracks + ' tracks');
+        numberOfTracks = Math.max(
+          0, Math.min(numberOfTracks, data.tracks.length));
+        console.log('returning ' + numberOfTracks + ' tracks');
+        console.log(data.tracks);
+        // TODO(peter): why not just take the first numberOfTracks items out of
+        // data.tracks?
+        for (var i = 0; i < numberOfTracks; i++) {
+          topTracks.push({
+            'name': data.tracks[i].name,
+            'id': data.tracks[i].id,
+          });
+        }
+        console.log(topTracks);
+        return topTracks;
+      });
+    });
+  }
+};
